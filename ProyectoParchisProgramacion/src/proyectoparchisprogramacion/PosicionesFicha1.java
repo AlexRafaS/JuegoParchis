4d52e0fba4b4d157155fe65f056e3584e7af47a3
AlexRafaS <56934822+AlexRafaS@users.noreply.github.com>
2023-10-29 22:23:00 -0600

    Update PosicionesFicha1.java

--- ProyectoParchisProgramacion/src/proyectoparchisprogramacion/PosicionesFicha1.java
+++ ProyectoParchisProgramacion/src/proyectoparchisprogramacion/PosicionesFicha1.java
@@ -1,22 +1,321 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyectoparchisprogramacion;
 
-import java.awt.List;
+import java.util.List;
 import java.util.ArrayList;
-
-
 /**
  *
  * @author alexs
  */
-public interface PosicionesFicha1 {
+public class PosicionesFicha1 {
     
+    /**
+     *
+     * @return
+     */
     
-    public List<CordenadasFichas1> listaPosiciones(){
-        List<CordenadasFichas1> lista = new Arra();
+    public Cordenada ObtenerCordenada(int posicion){
         
+        if(posicion == 1){
+            return new Cordenada(320,150);
+        }
+        
+        if(posicion == 2){
+            return new Cordenada(320,187);
+        }
+        
+        if(posicion == 3){
+            return new Cordenada(320,220);
+        }
+        
+         if(posicion == 4){
+            return new Cordenada(320,260);
+        }
+         
+          if(posicion == 5){
+            return new Cordenada(300,280);
+        }
+          
+        if(posicion == 6){
+            return new Cordenada(260,270);
+        }
+        
+        if(posicion == 7){
+            return new Cordenada(230,270);
+        }
+        
+                if(posicion == 8){
+            return new Cordenada(190,270);
+        }
+                
+                if(posicion == 9){
+            return new Cordenada(150,270);
+        }
+                
+                        if(posicion == 10){
+            return new Cordenada(110,270);
+        }
+        
+                if(posicion == 11){
+            return new Cordenada(80,270);
+        }
+        
+                if(posicion == 12){
+            return new Cordenada(40,270);
+        }
+        
+                if(posicion == 13){
+            return new Cordenada(40,370);
+        }
+                
+            if(posicion == 14){
+            return new Cordenada(40,460);
+        }
+                        
+            if(posicion == 15){
+            return new Cordenada(80,460);
+        }
+                                
+            if(posicion == 16){
+            return new Cordenada(120,460);
+        }
+                                        
+                if(posicion == 17){
+            return new Cordenada(150,460);
+        }
+                                                
+                if(posicion == 18){
+            return new Cordenada(190,460);
+        }
+              
+                if(posicion == 19){
+            return new Cordenada(230,460);
+        }
+                
+                        if(posicion == 20){
+            return new Cordenada(270,460);
+        }
+ 
+                if(posicion == 18){
+            return new Cordenada(300,460);
+        }
+                
+                if(posicion == 19){
+            return new Cordenada(330,480);
+        }
+
+                if(posicion == 20){
+            return new Cordenada(310,520);
+        }
+
+                if(posicion == 21){
+            return new Cordenada(310,560);
+        }
+
+                if(posicion == 22){
+            return new Cordenada(310,590);
+        }
+
+                if(posicion == 23){
+            return new Cordenada(310,630);
+        }
+
+                if(posicion == 24){
+            return new Cordenada(310,670);
+        }
+
+                if(posicion == 25){
+            return new Cordenada(310,710);
+        }
+
+                if(posicion == 26){
+            return new Cordenada(310,750);
+        }
+
+                if(posicion == 27){
+            return new Cordenada(410,750);
+        }
+
+                if(posicion == 28){
+            return new Cordenada(510,750);
+        }
+
+                if(posicion == 29){
+            return new Cordenada(510,710);
+        }
+
+                if(posicion == 30){
+            return new Cordenada(510,670);
+        }
+
+                if(posicion == 31){
+            return new Cordenada(510,630);
+        }
+
+                if(posicion == 32){
+            return new Cordenada(510,590);
+        }
+
+                if(posicion == 33){
+            return new Cordenada(510,560);
+        }
+
+                if(posicion == 34){
+            return new Cordenada(510,520);
+        }
+
+                if(posicion == 35){
+            return new Cordenada(510,490);
+        }
+
+                if(posicion == 36){
+            return new Cordenada(530,470);
+        }
+      
+  if(posicion == 37){
+            return new Cordenada(530,470);
+        }        
+
+    if(posicion == 38){
+            return new Cordenada(560,470);
+        }        
+    
+      if(posicion == 39){
+            return new Cordenada(600,470);
+        }        
+      
+        if(posicion == 40){
+            return new Cordenada(640,470);
+        }        
+        
+          if(posicion == 41){
+            return new Cordenada(680,470);
+        }        
+          
+            if(posicion == 42){
+            return new Cordenada(720,470);
+        }        
+            
+              if(posicion == 43){
+            return new Cordenada(750,470);
+        }        
+              
+                if(posicion == 44){
+            return new Cordenada(790,470);
+        }        
+                
+                  if(posicion == 45){
+            return new Cordenada(790,370);
+        }        
+                  
+                    if(posicion == 46){
+            return new Cordenada(790,270);
+        }        
+                    
+                      if(posicion == 47){
+            return new Cordenada(750,270);
+        }        
+                      
+                        if(posicion == 48){
+            return new Cordenada(720,270);
+        }        
+                        
+                          if(posicion == 49){
+            return new Cordenada(680,270);
+        }        
+                          
+                            if(posicion == 50){
+            return new Cordenada(640,270);
+        }        
+                            
+                              if(posicion == 51){
+            return new Cordenada(600,270);
+        }
+                              
+                              if(posicion == 52){
+            return new Cordenada(570,270);
+        }
+
+                              if(posicion == 53){
+            return new Cordenada(530,280);
+        }
+         
+          if(posicion == 54){
+            return new Cordenada(500,260);
+        }
+          
+            if(posicion == 55){
+            return new Cordenada(520,220);
+        }
+            
+              if(posicion == 56){
+            return new Cordenada(520,190);
+        }
+              
+          if(posicion == 57){
+            return new Cordenada(520,150);
+        }
+          
+          if(posicion == 58){
+            return new Cordenada(520,110);
+        }
+        
+          if(posicion == 59){
+            return new Cordenada(520,80);
+        }
+          
+            if(posicion == 60){
+            return new Cordenada(520,40);
+        }
+            
+              if(posicion == 61){
+            return new Cordenada(520,0);
+        }
+              
+                if(posicion == 62){
+            return new Cordenada(420,0);
+        }
+                
+         if(posicion == 63){
+            return new Cordenada(420,40);
+        }
+
+  if(posicion == 64){
+            return new Cordenada(420,80);
+        }
+  
+    if(posicion == 65){
+            return new Cordenada(420,110);
+        }
+    
+      if(posicion == 66){
+            return new Cordenada(420,150);
+        }
+      
+        if(posicion == 67){
+            return new Cordenada(420,190);
+        }
+        
+            if(posicion == 68){
+            return new Cordenada(420,220);
+        }
+            
+                    if(posicion == 69){
+            return new Cordenada(420,260);
+        }
+          
+                    
+                        if(posicion == 70){
+            return new Cordenada(420,310);
+        }
+                    
+          
+          
+          
+        return null;
     }
+    
 }
