13dced5266672847e059ae9cc0e978c96efd0db8
AlexRafaS <56934822+AlexRafaS@users.noreply.github.com>
2023-10-29 18:14:25 -0600

    primer commit

---
+++ ProyectoParchisProgramacion/src/proyectoparchisprogramacion/CordenadasFichas1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoparchisprogramacion;
+
+/**
+ *
+ * @author alexs
+ */
+public class CordenadasFichas1 {
+    int posicionFicha;
+    int posicionX;
+    int posicionY;
+    
+    
+    
+}
---
+++ ProyectoParchisProgramacion/src/proyectoparchisprogramacion/PosicionesFicha1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package proyectoparchisprogramacion;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author alexs
+ */
+public interface PosicionesFicha1 {
+    
+    
+    public List<CordenadasFichas1> listaPosiciones(){
+        List<CordenadasFichas1> lista = new Arra();
+        
+    }
+}
