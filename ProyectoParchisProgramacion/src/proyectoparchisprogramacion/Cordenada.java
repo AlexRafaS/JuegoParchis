054b7f9ee0b285e3fe87522ab739364880b0ba3c
AlexRafaS <56934822+AlexRafaS@users.noreply.github.com>
2023-10-29 21:45:52 -0600

    Create Cordenada.java

---
+++ ProyectoParchisProgramacion/src/proyectoparchisprogramacion/Cordenada.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoparchisprogramacion;
+
+/**
+ *
+ * @author alexs
+ */
+public class Cordenada {
+    int posicionx;
+    int posiciony;
+
+    public Cordenada(int posicionx, int posiciony) {
+        this.posicionx = posicionx;
+        this.posiciony = posiciony;
+    }
+    
+    
+    
+    
+}
